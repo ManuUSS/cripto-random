869328d264d523a39b4be4fdd5cc59a629c48acc
Manuel Ulate <fr3zs09@gmail.com>
2023-09-23 14:37:25 -0600

    fix: change on isLoading to isFetching

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export const App = () => {
   return (
     <div className='App App-header'>
       
-      { query.isLoading 
+      { query.isFetching 
         ? ( <h2>Cargando...</h2> ) 
         : ( <h2>Numero aleatorio: { query.data }</h2> )
       }
@@ -26,9 +26,9 @@ export const App = () => {
         !query.isLoading && query.isError && ( <h3>{ `${ query.error }` }</h3>)
       }
 
-      <button onClick={ () => query.refetch() } disabled={ query.isLoading }>
+      <button onClick={ () => query.refetch() } disabled={ query.isFetching }>
         {
-          query.isLoading ? '...' : 'Nuevo número'
+          query.isFetching ? '...' : 'Nuevo número'
         }
       </button>
 
