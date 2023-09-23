651693d437e5930a1b7babdd99942120a7e75448
Manuel Ulate <fr3zs09@gmail.com>
2023-09-23 14:34:37 -0600

    new: implementation of useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,34 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
 const getRandomNumber = async ():Promise<number> => {
   const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
   const number = await res.text();
-  // throw new Error('Se despi');
   return +number;
 }
 
 export const App = () => {
   
-  const [ number, setNumber ] = useState<number>();
-  const [ isLoading, setIsLoading ] = useState<boolean>( true );
-  const [ error, setError ] = useState<string>();
-  const [ key, forceRefetch ] = useReducer( ( x ) => x + 1 , 0 );
-
-  useEffect(() => {
-    setIsLoading( true );
-    getRandomNumber()
-      .then(setNumber )
-      .catch( error => setError( error.message ))
-  }, [ key ]);
-
-  useEffect(() => {
-    if( number ) setIsLoading( false );
-  }, [ number ]);
-  
-  useEffect(() => {
-    if( error ) setIsLoading( false );
-  }, [ error ])
-  
+  const query = useQuery(
+    ['randonNumber'],
+    getRandomNumber
+  );
 
   return (
     <div className='App App-header'>
       
-      { isLoading 
+      { query.isLoading 
         ? ( <h2>Cargando...</h2> ) 
-        : ( <h2>Numero aleatorio: { number }</h2> )
+        : ( <h2>Numero aleatorio: { query.data }</h2> )
       }
       
       {
-        !isLoading && error && ( <h3>{ error }</h3>)
+        !query.isLoading && query.isError && ( <h3>{ `${ query.error }` }</h3>)
       }
 
-      <button onClick={ forceRefetch } disabled={ isLoading }>
+      <button onClick={ () => query.refetch() } disabled={ query.isLoading }>
         {
-          isLoading ? '...' : 'Nuevo número'
+          query.isLoading ? '...' : 'Nuevo número'
         }
       </button>
 
